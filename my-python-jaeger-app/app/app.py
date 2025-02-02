553e36c0833a8b8ce081aa745b03eb723bee99cf
Renan <renan.de.souza@accenture.com>
2025-02-02 15:27:21 -0300

    changing name

--- my-python-jaeger-app/app/app.py
+++ my-python-jaeger-app/app/app.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 import sqlite3
-import os
 from prometheus_client import Counter, Histogram, Gauge
 from prometheus_flask_exporter import PrometheusMetrics
 from jaeger_client import Config
+from flask_opentracing import FlaskTracing
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
@@ -20,19 +21,6 @@ def get_post(post_id):
         abort(404)
     return post
 
-def init_tracer(service):
-    config = Config(
-        config={
-            'sampler': {
-                'type': 'const',
-                'param': 1,
-            },
-            'logging': True,
-        },
-        service_name=service,
-    )
-    return config.initialize_tracer()
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1234'
 metrics = PrometheusMetrics(app)
@@ -42,7 +30,23 @@ LATENCY = Histogram("http_request_duration_seconds", "Request latency in seconds
 ERRORS = Counter("http_request_errors_total", "Total number of request errors", ["error_type"])
 POSTS_COUNT = Gauge("posts_count", "Current number of posts")
 
+# Jaeger configuration
+def init_tracer(service):
+    config = Config(
+        config={
+            'sampler': {'type': 'const', 'param': 1},
+            'local_agent': {
+                'reporting_host': os.getenv('JAEGER_AGENT_HOST', 'jaeger'),
+                'reporting_port': os.getenv('JAEGER_AGENT_PORT', '6831'),
+            },
+            'logging': True,
+        },
+        service_name=service,
+    )
+    return config.initialize_tracer()
+
 tracer = init_tracer('my-python-jaeger-app')
+tracing = FlaskTracing(tracer, True, app)
 
 @app.route('/')
 def index():
